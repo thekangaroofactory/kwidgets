2b8a8bb52fe1699cdf2c25c851f2b4691dcae00c
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-02-22 11:46:16 +0100

    Move demo

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ library(bslib)
 
 # Define UI
 ui <- fluidPage(
-  
+
   tags$head(
     # Listen for update messages
     tags$script("
@@ -16,7 +16,7 @@ ui <- fluidPage(
         anim.beginElement()
       });
     "),
-    
+
     # Listen for update_bar messages
     tags$script("
     Shiny.addCustomMessageHandler('update_bar', function(value) {
@@ -28,43 +28,43 @@ ui <- fluidPage(
     });
     ")
     ),
-  
+
   # const gauge = document.getElementById('progress');
   # gauge.setAttribute('stroke', value['color']);
-  # 
-  
+  #
+
   # Application title
-  titlePanel("SVG Demo"),
-  
+  titlePanel("{kwidgets} demo"),
+
   layout_columns(
-    gauge(),  
+    gauge(),
     perf()),
-  
+
   actionButton(inputId = "click", label = "update value"),
   h1(textOutput("progress"))
-  
+
 )
 
 # Define server
 server <- function(input, output, session) {
-  
+
   old_value <- reactiveVal(180)
-  
+
   observeEvent(input$click, {
-    
+
     new_value <- sample(x = 0:100, size = 1)
     new_arc_value <- 2*pi * 48 * (100 - new_value) / 100
-    
+
     # color <- sample(x = c("grey", "beige", "cyan", "pink"), size = 1)
-    
+
     session$sendCustomMessage("update", new_arc_value)
     session$sendCustomMessage("update_bar", paste("M 0 50", new_value, "50"))
-    
+
     output$progress <- renderText(paste0(new_value, "%"))
-    
+
   })
-  
+
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
