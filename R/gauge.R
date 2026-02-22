59d68257654df1b5862cc090ee5daafd80bb6fec
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-02-22 11:44:59 +0100

    Add doc #6

--- R/gauge.R
+++ R/gauge.R
@@ -1,12 +1,19 @@
 
 
+#' Gauge Widget
+#'
+#' @returns an HTML widget
+#' @export
+#'
+#' @examples gauge()
+
 gauge <- function(){
-  
+
   div(style = "width: 300px;height: 300px;margin: 20px;",
-                 
+
       tags$svg(id="dummy",
                viewBox="0 0 100 100",
-               
+
                tags$circle(fill="none",
                            stroke="#aaa",
                            'stroke-width'="1",
@@ -15,7 +22,7 @@ gauge <- function(){
                            cy="50",
                            r="48",
                            'stroke-dasharray'="310"),
-               
+
                tags$circle(id = "progress",
                            fill="none",
                            stroke="#000",
@@ -27,7 +34,7 @@ gauge <- function(){
                            'stroke-dasharray'="301.5929",
                            'stroke-linecap'="round",
                            transform="rotate(-90 ) translate(-100 0)",
-                           
+
                            tags$animate(id="target",
                                         attributeName="stroke-dashoffset",
                                         from="301.5929",
@@ -37,6 +44,6 @@ gauge <- function(){
                                         keyTimes="0;1",
                                         calcMode="spline",
                                         repeatCount="1",
-                                        fill="freeze"))))      
-                
+                                        fill="freeze"))))
+
 }
