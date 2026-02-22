a619493f5bafee919eb9d6bcf6e1fe483565d0dd
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-02-22 14:24:27 +0100

    Add documentation #6

--- R/gauge.R
+++ R/gauge.R
@@ -1,6 +1,22 @@
 
-
 #' Gauge Widget
+#'
+#' @description
+#' An animated SVG gauge widget
+#'
+#' @param id a character string to specify the unique id for the widget
+#' @param wrap whether or not the widget should be wrapped into a div container
+#'
+#' @details
+#' `id` is used to compute ids for children nodes of the svg widget, pretty much
+#' like the namespace id in Shiny modules.
+#' To ensure uniqueness when used in Shiny modules, the id of the module instance
+#' should be used as a prefix `id = paste(id, "gauge", sep = "-)`.
+#'
+#' The div container used when `wrap = TRUE` comes with basic style (width: 300px,
+#' height: 300px, margin: 20px) to avoid the widget to occupy the whole page.
+#' If you wish to specify other values or options, set `wrap = FALSE` and encapsulate
+#' the widget in your own div.
 #'
 #' @returns an HTML widget
 #' @export
