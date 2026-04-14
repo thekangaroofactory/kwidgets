067cb9729df872b235fbfe1d4da8affc379f14fe
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-04-14 14:53:40 +0200

    progress #21

--- R/progress_update.R
+++ R/progress_update.R
@@ -5,6 +5,7 @@
 #' Check if the required dependency needs to be inserted on UI side
 #' and update the progress widget.
 #'
+#' @param id the widget to update
 #' @param value the new progress value to display
 #' @param session the session of object of the server function
 #'
@@ -15,7 +16,7 @@
 #' progress_update(value = 52)
 #' }
 
-progress_update <- function(value, session = shiny::getDefaultReactiveDomain()){
+progress_update <- function(id, value, session = shiny::getDefaultReactiveDomain()){
 
   # -- insert javascript dependency
   ktools::with_js(package = "kwidgets",
@@ -23,6 +24,8 @@ progress_update <- function(value, session = shiny::getDefaultReactiveDomain()){
                   script = "js/progress.js")
 
   # -- call update
-  session$sendCustomMessage("update_progress", paste("M 0 50", value, "50"))
+  session$sendCustomMessage("update_progress",
+                            list(id = id,
+                                 progress = paste("M 0 50", value, "50")))
 
 }
--- app.R
+++ app.R
@@ -10,8 +10,9 @@ ui <- page_fluid(
 
   layout_column_wrap(
     gauge(id = "w1", options = list(color = "#710293")),
-    gauge(id = "w3", options = list(color = "#214999")),
-    progress(id = "w2")),
+    gauge(id = "w2", options = list(color = "#214999")),
+    progress(id = "w3"),
+    progress(id = "w4")),
 
   actionButton(inputId = "click", label = "update value"),
   h1(textOutput("progress"))
@@ -30,8 +31,9 @@ server <- function(input, output, session) {
 
     # -- update widgets
     gauge_update(id = "w1", new_value)
-    gauge_update(id = "w3", new_value / 2)
-    progress_update(new_value)
+    gauge_update(id = "w2", new_value / 2)
+    progress_update(id = "w3", new_value)
+    progress_update(id = "w4", new_value / 2)
 
     # -- display value
     output$progress <- renderText(paste0(new_value, "%"))
